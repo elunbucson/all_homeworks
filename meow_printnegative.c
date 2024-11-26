470e5506522ae2d6229a541bbb5389ab191ae89e
elunbucson <antomoravilla050811@gmail.com>
2024-11-26 00:11:54 -0800

    octavo commit

---
+++ meow_printnegative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
+
+    
+    if (numero < 0) {
+        printf("Meow\n");
+    }
+
+    return 0;
+}
